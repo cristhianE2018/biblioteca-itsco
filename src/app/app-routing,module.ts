b14c01c04170b065d9bbf1c0e86e5aefcaabd9af
crisOlivares <cristhian@olivares.com>
2023-05-15 12:13:05 -0600

    estructura inicial

---
+++ src/app/app-routing,module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LibrosComponent } from "./libros/libros.component";
+const routes: Routes = [
+
+  {
+    path: 'libros',
+    component: LibrosComponent
+  }
+
+]
